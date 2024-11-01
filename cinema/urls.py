84f45ac3da47046db792c487d5cea6dadb8edd70
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-01 13:52:02 +0200

    Added pathes for genre-list and denre-detail.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    genre_list,
+    genre_detail,
+    movie_list,
+    movie_detail
+)
 
 urlpatterns = [
+    path("genres/", genre_list, name="genre-list"),
+    path("genres/<int:pk>/", genre_detail, name="genre-detail"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
