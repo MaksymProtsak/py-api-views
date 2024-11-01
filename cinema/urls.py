09c493fccc543d437df6562380f2e31530c2bf3d
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-01 16:43:16 +0200

    Created cinema_halls-list, cinema_halls-detail. Created rourer and added movies to router.register. Replace movie-list, movie-detail. Added '' path with include router.urls.

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,21 +1,46 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
     genre_list,
     genre_detail,
     ActorList,
     ActorDetail,
-    movie_list,
-    movie_detail
+    CinemaHallViewSet,
+    MovieListViewSet,
 )
+
+router = routers.DefaultRouter()
+router.register("movies", MovieListViewSet)
 
 urlpatterns = [
     path("genres/", genre_list, name="genre-list"),
     path("genres/<int:pk>/", genre_detail, name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
     path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path(
+        "cinema_halls/",
+        CinemaHallViewSet.as_view(
+            actions={
+                "get": "list",
+                "post": "create",
+            }
+        ),
+        name="cinema_halls-list"
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        CinemaHallViewSet.as_view(
+            actions={
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="cinema_halls-detail"
+    ),
+    path("", include(router.urls))
 ]
 
 app_name = "cinema"
