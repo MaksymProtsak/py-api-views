f11e7e01b5f7ce40a160ac012afdd3fe4b3cc5fb
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-01 14:04:03 +0200

    Created pathes for actor-list, actor-detail.

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from cinema.views import (
     genre_list,
     genre_detail,
+    ActorList,
+    ActorDetail,
     movie_list,
     movie_detail
 )
@@ -10,6 +12,8 @@ from cinema.views import (
 urlpatterns = [
     path("genres/", genre_list, name="genre-list"),
     path("genres/<int:pk>/", genre_detail, name="genre-detail"),
+    path("actors/", ActorList.as_view(), name="actor-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
 ]
